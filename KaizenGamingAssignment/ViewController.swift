a06a089e8723e000a62a2e23dad39fc79c45a6c7
Huseyin Yusuf <hy@wappier.com>
2023-06-01 14:21:04 +0300

    Initialize project

---
+++ KaizenGamingAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaizenGamingAssignment
+//
+//  Created by Huseyin Yusuf on 1/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
