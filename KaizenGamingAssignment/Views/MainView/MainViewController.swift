863b080c2e7c9d7ac25adc71f7f8f4f2c82e7369
Huseyin Yusuf <hy@wappier.com>
2023-06-02 19:53:28 +0300

    Complete full version of navigation bar with custimization

--- KaizenGamingAssignment/Views/MainView/MainViewController.swift
+++ KaizenGamingAssignment/Views/MainView/MainViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set up at first load
+        setup()
     }
-
-
+    
+    // MARK: - Private methods
+    private func setup() {
+        // Setup navigation bar title
+        setupNavigationBarTitle()
+    }
+    
+    private func setupNavigationBarTitle() {
+        let titleImage = UIImage(named: "kaizen_gaming_navbar")
+        let titleImageView = UIImageView(image: titleImage)
+        titleImageView.contentMode = .scaleAspectFit
+        self.navigationItem.titleView = titleImageView
+    }
+    
+    
+    // MARK: - Navigation Bar Actions
+    @IBAction func porfileButtonAction(_ sender: Any) {
+        // Profile button action should be implemented here
+    }
+    
+    @IBAction func settingsButtonAction(_ sender: Any) {
+        // Settings button action should be implemented here
+    }
 }
 
